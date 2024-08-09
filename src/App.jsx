25eb0cdfae01c65bfa8892fdf97be6af0ea006ae
walidlido11 <futlido11@gmail.com>
2024-08-09 21:14:45 +0300

    Update component and other changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Register from './components/Auth/Register';
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard';
 import AssignRolePage from './pages/AssignRolePage'; // Ensure correct import
 import ProtectedRoute from './routes/ProtectedRoute';
-import RBBotAccounts from './components/RBBotAccount/RBBotAccountsPage';
+import RBBotAccountsPage from './components/RBBotAccount/RBBotAccountsPage';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
