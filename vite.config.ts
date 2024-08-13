c860038795f81c64eeeb5b2d41ca7edac304f563
walidlido11 <futlido11@gmail.com>
2024-08-13 04:33:39 +0300

    Update component and other changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 // vite.config.js
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000', // تأكد من تطابق المنفذ مع المنفذ الذي يعمل عليه الخادم
+      '/api': {
+        target: 'https://gamehub-backend-5c3f456a5ad4.herokuapp.com', // عنوان خادم Heroku
+        changeOrigin: true,
+        secure: true, // استخدم true إذا كنت تستخدم HTTPS
+      },
     },
   },
 });
